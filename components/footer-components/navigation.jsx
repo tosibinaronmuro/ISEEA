c923883d2edf79b085eb203b2cf14a9942c0ea4b
tosiron <tosironj@gmail.com>
2023-07-03 21:41:36 +0100

    added home sextion to the navigation

--- components/footer-components/navigation.jsx
+++ components/footer-components/navigation.jsx
@@ -3,22 +3,32 @@ import React from 'react'
 const Navigation = () => {
   return (
     <ul
-    className="mt-12 flex flex-wrap justify-center gap-6 md:gap-8 lg:mt-0 lg:justify-end lg:gap-12"
+    className="mt-12 flex flex-wrap justify-center gap-6 md:gap-6 lg:mt-0 lg:justify-end lg:gap-6"
   >
     <li>
-      <a className="text-gray-700 transition hover:text-gray-700/75" target='_blank' href="/">
+      <a className="text-gray-700 transition hover:text-gray-700/75"  href="/about">
         About
       </a>
     </li>
 
     <li>
-      <a className="text-gray-700 transition hover:text-gray-700/75" target='_blank' href="/">
-        Services
+      <a className="text-gray-700 transition hover:text-gray-700/75"  href="/stories">
+        Stories
       </a>
     </li>
 
     <li>
-      <a className="text-gray-700 transition hover:text-gray-700/75" target='_blank' href="/">
+      <a className="text-gray-700 transition hover:text-gray-700/75"  href="/history">
+        History
+      </a>
+    </li>
+    <li>
+      <a className="text-gray-700 transition hover:text-gray-700/75"  href="/team">
+        Team
+      </a>
+    </li>
+    <li>
+      <a className="text-gray-700 transition hover:text-gray-700/75"  href="/">
         Projects
       </a>
     </li>
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -55,6 +55,14 @@ export default function Navbar({ fixed }) {
               <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
                 <a
                   className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-3 py-2 flex"
+                  href="/"
+                >
+                  Home
+                </a>
+              </li>
+              <li className=" w-full lg:w-auto transition ease-in-out delay-150 text-tertiary hover:-translate-y-1 hover:scale-105 hover:text-tertiary  duration-300">
+                <a
+                  className=" lg:inline-flex lg:w-auto w-full   rounded items-center   text-tertiary transition hover:text-tertiary/75 px-3 py-2 flex"
                   href="/about"
                 >
                   About

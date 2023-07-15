c9ccf1b0448a9b046f6a1bd8d1b2d4328e5664cc
tosiron <tosironj@gmail.com>
2023-07-15 16:48:26 +0100

    minor UI changes

--- app/history/page.jsx
+++ app/history/page.jsx
@@ -49,7 +49,7 @@ const History = () => {
           </p>
         </div> */}
         <div className="bg-blackRgba w-[80%]   blackRgba h-[40vh] lg:h-[60vh] flex flex-col justify-center items-center  absolute bottom-0 left-50 p-2  ">
-        <div className="w-[55%] h-auto lg:mt-44   ">
+        <div className="lg:w-[55%] w-[90%] h-auto lg:mt-44   ">
             <p className="flex justify-start font-garmond text-base font-bold lg:text-4xl  text-white">
               Here is a recap of our journey
             </p>
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -39,7 +39,7 @@ const Projects = () => {
           </p>
         </div> */}
         <div className="bg-blackRgba w-[80%]   blackRgba h-[40vh] lg:h-[60vh] flex flex-col justify-center items-center  absolute bottom-50 left-50 p-2  ">
-        <div className="w-[55%] h-auto lg:mt-44   ">
+        <div className="lg:w-[55%] w-[90%] h-auto lg:mt-44   ">
           <p className="flex justify-start font-garmond text-base font-bold lg:text-4xl  text-white">
           Projects and Activities
           </p>

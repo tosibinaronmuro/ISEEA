55187cd71ef63395d0fb1824b7394f0222360494
tosiron <tosironj@gmail.com>
2023-08-18 23:13:47 +0100

    minor fix

--- components/donate-components/bank.jsx
+++ components/donate-components/bank.jsx
@@ -65,42 +65,42 @@ const BankDetails = ({ BankName, AccountName, AccountNumber, Tag }) => {
                 opacity="0.4"
                 d="M17 13.4V16.4C17 20.4 15.4 22 11.4 22H7.6C3.6 22 2 20.4 2 16.4V12.6C2 8.6 3.6 7 7.6 7H10.6"
                 stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M16.9996 13.4H13.7996C11.3996 13.4 10.5996 12.6 10.5996 10.2V7L16.9996 13.4Z"
                 stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 opacity="0.4"
                 d="M7 5C7 3.34 8.34 2 10 2H16"
                 stroke="#FCC200"
                 fill="FCC200"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 opacity="0.4"
                 d="M22.0004 8V14.19C22.0004 15.74 20.7404 17 19.1904 17"
                 stroke="#FCC200"
                 fill="FCC200"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
               <path
                 d="M22 8H19C16.75 8 16 7.25 16 5V2L22 8Z"
                 stroke="#FCC200"
                 fill="FCC200"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               />
             </svg>
           </div>

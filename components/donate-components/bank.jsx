a1288ced78b2da86d776182b81a541adf714d467
tosiron <tosironj@gmail.com>
2023-08-18 22:02:13 +0100

    minor icon change

--- components/donate-components/bank.jsx
+++ components/donate-components/bank.jsx
@@ -31,8 +31,7 @@ const BankDetails = ({ BankName, AccountName, AccountNumber, Tag }) => {
             className="cursor-pointer mx-2 h-7 w-7"
             viewBox="0 0 384 512"
           >
-            <path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z" />
-          </svg>
+          <path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>
         </div>
         <p className="font-light font-mono text-xl text-gray-700 bg-white px-4 w-fit rounded-lg hover:text-gray-900 transition-all duration-200">
           {AccountNumber}
